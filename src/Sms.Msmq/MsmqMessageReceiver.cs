5faf8d1100402db73576b4ba6e6f6ba980131639
markkemper1 <markkemper1@gmail.com>
2015-02-12 15:19:05 +1100

    Modified msmq to discad (and log) corrupted messages

--- src/Sms.Msmq/MsmqMessageReceiver.cs
+++ src/Sms.Msmq/MsmqMessageReceiver.cs
@@ -1,117 +1,172 @@
 ﻿using System;
+using System.IO;
 using System.Messaging;
 using System.Threading;
 using Sms.Messaging;
 
 namespace Sms.Msmq
 {
-    public class MsmqMessageReceiver : IReceiver, IDisposable
-    {
-        readonly MessageQueue messageQueue = null;
-        //private bool stopping;
-        private readonly string queueName;
 
-        //public bool Receiving { get; private set; }
+	public class XmlWrappedMessageFormatter : IMessageFormatter
+	{
+		private IMessageFormatter xmlFommater = new XmlMessageFormatter(new Type[1] { typeof(SmsMessageContent) });
 
-        public string ProviderName { get; private set; }
+		public object Clone()
+		{
+			return new XmlWrappedMessageFormatter();
+		}
 
-        public MsmqMessageReceiver(string providerName, string queueName)
-        {
-            ProviderName = providerName;
-            QueueName = queueName;
-            this.queueName = @".\Private$\" + queueName;
+		public bool CanRead(Message message)
+		{
+			return true;
+		}
 
-            EnsureQueueExists.OfName(this.queueName);
+		public object Read(Message message)
+		{
+			try
+			{
+				return xmlFommater.Read(message);
+			}
+			catch (Exception ex)
+			{
+				string messageContent = null;
+				if(message.BodyStream != null && message.BodyStream.CanSeek)
+				{
+					message.BodyStream.Position = 0;
+					using(var reader = new StreamReader(message.BodyStream))
+					{
+						messageContent = reader.ReadToEnd();
+					}
+				}
 
-            
-            messageQueue = new MessageQueue(this.queueName);
-            messageQueue.Formatter = new XmlMessageFormatter(new Type[1] { typeof(SmsMessageContent) });
-        }
+				if (messageContent != null) ;
+				Logger.Error(messageContent);
+				Logger.Error("Msmq receiver: message body could not be serialized. Message Dropped!. " + ex.ToString());
 
-        public void Dispose()
-        {
-            messageQueue.Dispose();
-        }
+				return null;
+			}
+		}
 
-        public string QueueName { get; private set; }
+		public void Write(Message message, object obj)
+		{
+			throw new NotImplementedException();
+		}
+	}
+	public class MsmqMessageReceiver : IReceiver, IDisposable
+	{
+		readonly MessageQueue messageQueue = null;
+		//private bool stopping;
+		private readonly string queueName;
 
-        public MessageResult Receive(TimeSpan? timeout = null)
-        {
-            MessageQueueTransaction transaction = null;
-            try
-            {
-                try
-                {
-                    transaction = new MessageQueueTransaction();
-                    transaction.Begin();
-                    using (
-                        var raw = timeout.HasValue
-                                      ? messageQueue.Receive(timeout.Value, transaction)
-                                      : messageQueue.Receive(transaction))
-                    {
+		//public bool Receiving { get; private set; }
 
-                        var message = ((SmsMessageContent) raw.Body).ToMessage();
+		public string ProviderName { get; private set; }
 
-                        Func<MessageQueueTransaction, Action<bool>> onReceive = queueTransaction =>
-                            {
-                                Action<bool> handler = x =>
-                                    {
-                                        if (queueTransaction.Status == MessageQueueTransactionStatus.Pending)
-                                        {
-                                            if (x)
-                                                queueTransaction.Commit();
-                                            else
-                                                queueTransaction.Abort();
-                                        }
+		public MsmqMessageReceiver(string providerName, string queueName)
+		{
+			ProviderName = providerName;
+			QueueName = queueName;
+			this.queueName = @".\Private$\" + queueName;
 
-                                        queueTransaction.Dispose();
-                                    };
-                                return handler;
-                            };
+			EnsureQueueExists.OfName(this.queueName);
 
-                        return new MessageResult(message, onReceive(transaction));
-                    }
-                }
-                catch (MessageQueueException ex)
-                {
-                    TryToAbortTransaction(transaction);
+			messageQueue = new MessageQueue(this.queueName);
+			messageQueue.Formatter = new XmlWrappedMessageFormatter();
+		}
 
-                    if (ex.Message == "Timeout for the requested operation has expired.")
-                    {
-                        Logger.Debug("Msmq receiver: timeout while waiting for message (this is ok)");
-                        return null;
-                    }
-                    int sleepForMs = timeout.HasValue ? timeout.Value.Milliseconds : 500;
-                    Logger.Warn("Msmq receiver: message queue exception on receive, sleeping for {0} Details: {1}",
-                                sleepForMs, ex);
-                    Thread.Sleep(sleepForMs);
-                    return null;
-                }
-            }
-            catch (Exception ex)
-            {
-                TryToAbortTransaction(transaction);
-                Logger.Warn("Msmq receiver: unknow error: {0}", ex);
-                throw;
-            }
-        }
+		public void Dispose()
+		{
+			messageQueue.Dispose();
+		}
 
-        private void TryToAbortTransaction(MessageQueueTransaction transaction)
-        {
-            if (transaction != null)
-            {
-                try
-                {
-                    transaction.Abort();
-                    transaction.Dispose();
-                    transaction = null;
-                }
-                catch (Exception ex)
-                {
-                    Logger.Warn("Msmq receiver: error shutting down transaction: {0}", ex);
-                }
-            }
-        }
+		public string QueueName { get; private set; }
 
-    }
+		public MessageResult Receive(TimeSpan? timeout = null)
+		{
+			MessageQueueTransaction transaction = null;
+			try
+			{
+				try
+				{
+					transaction = new MessageQueueTransaction();
+					transaction.Begin();
+					using (
+						var raw = timeout.HasValue
+									  ? messageQueue.Receive(timeout.Value, transaction)
+									  : messageQueue.Receive(transaction))
+					{
+						var smsMessageContent = ((SmsMessageContent)raw.Body);
+
+						if (smsMessageContent == null)
+						{
+							transaction.Commit();
+							return null;
+						}
+						else
+						{
+							var message = smsMessageContent.ToMessage();
+
+							Func<MessageQueueTransaction, Action<bool>> onReceive = queueTransaction =>
+							{
+								Action<bool> handler = x =>
+								{
+									if (queueTransaction.Status == MessageQueueTransactionStatus.Pending)
+									{
+										if (x)
+											queueTransaction.Commit();
+										else
+											queueTransaction.Abort();
+									}
+
+									queueTransaction.Dispose();
+								};
+								return handler;
+							};
+
+							return new MessageResult(message, onReceive(transaction));
+						}
+					}
+				}
+				catch (MessageQueueException ex)
+				{
+					TryToAbortTransaction(transaction);
+
+					if (ex.Message == "Timeout for the requested operation has expired.")
+					{
+						Logger.Debug("Msmq receiver: timeout while waiting for message (this is ok)");
+						return null;
+					}
+					int sleepForMs = timeout.HasValue ? timeout.Value.Milliseconds : 500;
+					Logger.Warn("Msmq receiver: message queue exception on receive, sleeping for {0} Details: {1}",
+								sleepForMs, ex);
+					Thread.Sleep(sleepForMs);
+					return null;
+				}
+			}
+			catch (Exception ex)
+			{
+				TryToAbortTransaction(transaction);
+				Logger.Warn("Msmq receiver: unknow error: {0}", ex);
+				throw;
+			}
+		}
+
+		private void TryToAbortTransaction(MessageQueueTransaction transaction)
+		{
+			if (transaction != null)
+			{
+				try
+				{
+					transaction.Abort();
+					transaction.Dispose();
+					transaction = null;
+				}
+				catch (Exception ex)
+				{
+					Logger.Warn("Msmq receiver: error shutting down transaction: {0}", ex);
+				}
+			}
+		}
+
+	}
 }
